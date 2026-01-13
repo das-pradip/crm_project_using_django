2559cfe59c008b8996aa16d8fbf185f6d54e74bb
Pradip Das <108473592+das-pradip@users.noreply.github.com>
2026-01-14 01:10:56 +0530

    Secure settings and ignore env & cache files

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
