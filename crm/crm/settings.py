7e14b0765514826e1e1b837ccd30be20a5b83145
Pradip Das <108473592+das-pradip@users.noreply.github.com>
2026-01-14 22:34:57 +0530

    Remove WhiteNoise and revert static settings

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -67,8 +67,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    # 'django.middleware.security.SecurityMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'crm.urls'
@@ -142,8 +142,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # MEDIA_ROOT = BASE_DIR / 'media'
