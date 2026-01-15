efe04e6a8f4d42b99851e69167cc126a6a2f5a5e
Pradip Das <108473592+das-pradip@users.noreply.github.com>
2026-01-16 00:16:10 +0530

    Stop sending mail after login

--- crm/webapp/signals.py
+++ crm/webapp/signals.py
@@ -7,26 +7,26 @@ from django.conf import settings
 from .models import UserProfile
 
 
-@receiver(user_logged_in)
-def send_login_email(sender, request, user, **kwargs):
-    subject = "Login Alert - CRM System"
-    message = f"""
-Hello {user.username},
+# @receiver(user_logged_in)
+# def send_login_email(sender, request, user, **kwargs):
+#     subject = "Login Alert - CRM System"
+#     message = f"""
+# Hello {user.username},
 
-You have successfully logged in to the CRM system.
+# You have successfully logged in to the CRM system.
 
-If this was not you, please contact support immediately.
+# If this was not you, please contact support immediately.
 
-Thank you,
-CRM Team
-"""
-    send_mail(
-        subject,
-        message,
-        settings.DEFAULT_FROM_EMAIL,
-        [user.email],
-        fail_silently=False,
-    )
+# Thank you,
+# CRM Team
+# """
+#     send_mail(
+#         subject,
+#         message,
+#         settings.DEFAULT_FROM_EMAIL,
+#         [user.email],
+#         fail_silently=False,
+#     )
 
 
 @receiver(post_save, sender=User)
