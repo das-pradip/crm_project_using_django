4648f80fed4a7c6234875e3c9c99b28663fe4691
Pradip Das <108473592+das-pradip@users.noreply.github.com>
2026-01-17 00:35:28 +0530

    Fix superuser creation

--- crm/createsuperuser.py
+++ crm/createsuperuser.py
@@ -3,17 +3,25 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+print("Running createsuperuser script...")
+
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-if username and password:
-    if not User.objects.filter(username=username).exists():
+print("Username:", username)
+print("Email:", email)
+
+if not username or not password:
+    print("ENV variables missing")
+else:
+    if User.objects.filter(username=username).exists():
+        print("Superuser already exists")
+    else:
         User.objects.create_superuser(
             username=username,
             email=email,
             password=password
         )
-        print("Superuser created")
-    else:
-        print("Superuser already exists")
+        print("Superuser CREATED successfully")
+
