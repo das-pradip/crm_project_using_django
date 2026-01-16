af17a6a1e6361fd6b0d064caf1b72d29457aa1e3
Pradip Das <108473592+das-pradip@users.noreply.github.com>
2026-01-16 16:07:43 +0530

    Change in SECRECT KEY

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -149,6 +149,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 if DEBUG: 
     STATICFILES_DIRS = [BASE_DIR / "static"]
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
